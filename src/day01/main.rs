4fccc80471653f176910423141b4e0f50c55a473
Aris Poloway <agpoloway@gmail.com>
2021-12-01 18:48:45 -0500

    Rust

---
+++ src/day01/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+
+fn main() {
+    let mut int_lines: Vec<i32> = Vec::new();
+    for line in lines("day01/in.txt") {
+        int_lines.push(line.parse().unwrap());
+    }
+
+    let mut count = -1;
+    let mut last = 0;
+
+    for line in &int_lines {
+        if line > &last {
+            count += 1;
+        }
+        last = *line;
+    }
+
+    println!("{}", count);
+
+    let mut count = -1;
+    let mut last = 0;
+
+    for index in 0..(int_lines.len() - 2) {
+        let sum = int_lines[index..index + 3].iter().sum();
+        if sum > last {
+            count += 1;
+        }
+        last = sum;
+    }
+    println!("{}", count);
+}
+
+fn lines(filename: &str) -> Vec<String> {
+    let file_string = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    file_string
+        .split("\n")
+        .map(|x| x.trim_end().to_string())
+        .collect()
+}
