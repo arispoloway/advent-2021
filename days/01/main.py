4e9a24564827c565d52c6dccb2597082ac41e187
Aris Poloway <agpoloway@gmail.com>
2021-12-01 00:14:27 -0500

    python solution

--- days/01/main.py
+++ days/01/main.py
@@ -1,10 +1,25 @@
 from collections import defaultdict, Counter
 
 def main(lines):
+  last = 0
+  count = -1
   for line in lines:
-    print(line)
+    new = int(line)
+    if new > last:
+      count += 1
+    last = new
+  print(count)
+
+  last = 0
+  count = -1
+  for i in range(len(lines) - 2):
+    new = sum(map(int, lines[i:i+3]))
+    if new > last:
+      count += 1
+    last = new
+  print(count)
 
 if __name__ == '__main__':
   with open('in.txt') as f:
-    main(map(lambda x: x.rstrip('\n'), f.readlines()))
+    main(list(map(lambda x: x.rstrip('\n'), f.readlines())))
 
