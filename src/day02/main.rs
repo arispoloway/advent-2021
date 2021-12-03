d23a9525e2856ea417aafe0f458d72fce8e501e4
Aris Poloway <agpoloway@gmail.com>
2021-12-02 19:43:50 -0500

    Refactoring

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -1,89 +1,79 @@
 use std::fs;
 
-#[derive(Debug, Copy, Clone)]
-enum Direction {
-    Forward,
-    Down,
-    Up
+type Input = Vec<Action>;
+const INPUT_FILE: &str = "inputs/02.txt";
+
+fn parse_lines(lines: Vec<String>) -> Input {
+    let mut actions: Vec<Action> = Vec::new();
+
+    for line in lines {
+        let action = Action::parse(&line);
+        actions.push(action);
+    }
+
+    actions
 }
 
-impl Direction {
-    fn parse(dir: &str) -> Direction {
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    Forward(i32),
+    Down(i32),
+    Up(i32)
+}
+
+impl Action {
+    fn parse(line: &String) -> Action {
+        let mut split = line.split(" ");
+        let dir = split.next().unwrap();
+        let amount = split.next().unwrap().parse::<i32>().unwrap();
         if dir == "forward" {
-            Direction::Forward {}
+            Action::Forward(amount)
         } else if dir == "down" {
-            Direction::Down {}
+            Action::Down(amount)
         } else if dir == "up" {
-            Direction::Up {}
+            Action::Up(amount)
         } else {
             panic!("Unrecognized direction")
         }
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Action {
-    direction: Direction,
-    amount: i32
-}
-
-fn part1(actions: &Vec<Action>) -> i64 {
+fn part1(input: &Input) -> String {
     let mut distance: i64 = 0;
     let mut depth: i64 = 0;
 
-    for action in actions {
+    for action in input {
         match action {
-            Action { direction: Direction::Up, amount } => depth = depth - (*amount as i64),
-            Action { direction: Direction::Down, amount } => depth = depth + (*amount as i64),
-            Action { direction: Direction::Forward, amount } => distance = distance + (*amount as i64),
+            Action::Up(amount) => depth = depth - (*amount as i64),
+            Action::Down(amount) => depth = depth + (*amount as i64),
+            Action::Forward(amount) => distance = distance + (*amount as i64),
         }
     }
 
-    distance * depth
+    format!("{}", distance * depth)
 }
 
-fn part2(actions: &Vec<Action>) -> i64 {
+fn part2(input: &Input) -> String {
     let mut aim: i64 = 0;
     let mut distance: i64 = 0;
     let mut depth: i64 = 0;
 
-    for action in actions {
+    for action in input {
         match action {
-            Action { direction: Direction::Up, amount } => aim = aim - (*amount as i64),
-            Action { direction: Direction::Down, amount } => aim = aim + (*amount as i64),
-            Action { direction: Direction::Forward, amount } => {
+            Action::Up(amount) => aim = aim - (*amount as i64),
+            Action::Down(amount) => aim = aim + (*amount as i64),
+            Action::Forward(amount) => {
                 distance = distance + (*amount as i64);
                 depth = depth + (aim * (*amount as i64));
             },
         }
     }
 
-    distance * depth
+    format!("{}", distance * depth)
 }
 
-fn main() {
-    let actions = parse_input();
-
-    println!("{}", part1(&actions));
-    println!("{}", part2(&actions));
-}
-
-fn parse_input() -> Vec<Action> {
-    parse_lines(lines("inputs/02.txt"))
-}
-
-fn parse_lines(lines: Vec<String>) -> Vec<Action> {
-    let mut actions: Vec<Action> = Vec::new();
-
-    for line in lines {
-        let test: Vec<String> = line.split(" ").map(|x| x.to_string()).collect();
-        let direction = Direction::parse(&test[0]);
-        let amount = test[1].parse::<i32>().unwrap();
-        let action = Action { direction, amount };
-        actions.push(action);
-    }
-
-    actions
+fn parse_input() -> Input {
+    parse_lines(lines(INPUT_FILE))
 }
 
 fn lines(filename: &str) -> Vec<String> {
@@ -93,4 +83,11 @@ fn lines(filename: &str) -> Vec<String> {
         .split("\n")
         .map(|x| x.trim_end().to_string())
         .collect()
+}
+
+fn main() {
+    let actions = parse_input();
+
+    println!("{}", part1(&actions));
+    println!("{}", part2(&actions));
 }
